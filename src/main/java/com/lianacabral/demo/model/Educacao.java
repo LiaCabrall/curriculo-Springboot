92532d0c64eb76cb01471e51f89fa27451b52568
LiaCabrall <lianav.cabral@gmail.com>
2025-04-15 12:09:53 -0300

    Variavel de ambiente

--- src/main/java/com/lianacabral/demo/model/Educacao.java
+++ src/main/java/com/lianacabral/demo/model/Educacao.java
@@ -1,7 +1,6 @@
 package com.lianacabral.demo.model;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
